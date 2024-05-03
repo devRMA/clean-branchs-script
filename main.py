4d9d268cd3d30b144b638d0be56878bd5e7fdca0
Rafael <rafael271alves@gmail.com>
2024-05-03 15:35:40 -0300

    added git switch to default branch

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from subprocess import run, check_output
+from subprocess import run, check_output, DEVNULL
+
+def run_command(command: list[str]) -> None:
+    """Run a command in the terminal without showing the output
+
+    Parameters
+    ----------
+    command : list[str]
+        The command to run
+    """
+    run(command, stdout = DEVNULL, stderr = DEVNULL)
 
 def create_branches(amount: int = 10, prefix: str = "branch") -> None:
     """Create branches with a prefix and push them to origin
@@ -12,11 +22,11 @@ def create_branches(amount: int = 10, prefix: str = "branch") -> None:
     """
     for i in range(amount):
         branch_name = f"{prefix}{i+1:02d}"
-        run(['git', 'checkout', '-b', branch_name])
-        run(['git', 'push', 'origin', branch_name])
+        run_command(['git', 'checkout', '-b', branch_name])
+        run_command(['git', 'push', 'origin', branch_name])
         print(f"Created {branch_name} branch and pushed to origin")
 
-def get_branches()-> list[str]:
+def get_branches(raw: bool = False)-> list[str]:
     """Get the branches from the remote repository
 
     Returns
@@ -26,7 +36,22 @@ def get_branches()-> list[str]:
     """
     output = check_output(['git', 'branch', '-r']).decode().splitlines()
 
-    return [branch.split('/')[-1].strip() for branch in output]
+    return [branch if raw else branch.split('/')[-1].strip() for branch in output]
+
+def get_default_branch() -> str:
+    """Get the default branch of the remote repository
+
+    Returns
+    -------
+    str
+        The default branch name
+    """
+    branches = get_branches(True)
+    for branch in branches:
+        if 'HEAD -> ' in branch:
+            return branch.split('/')[-1].strip()
+
+    return 'main'
 
 def delete_branches(branches_to_keep: list[str] = ['main', 'master']) -> None:
     """Delete branches from the remote repository
@@ -37,12 +62,21 @@ def delete_branches(branches_to_keep: list[str] = ['main', 'master']) -> None:
         The branches to keep, by default ['main', 'master']
     """
     branches = get_branches()
+    deleted_branches = 0
 
     for branch in branches:
         if branch not in branches_to_keep:
-            run(['git', 'push', 'origin', '--delete', branch])
-            run(['git', 'branch', '-d', branch])
+            try:
+                run_command(['git', 'push', 'origin', '--delete', branch])
+                run_command(['git', 'branch', '-d', branch])
+            except:
+                print(f"Failed to delete {branch} branch from origin")
+            finally:
+                run_command(['git', 'switch', get_default_branch()])
             print(f"Deleted {branch} branch from origin")
+            deleted_branches += 1
+        
+    print(f"Deleted {deleted_branches} branches")
 
 if __name__ == "__main__":
     ...
